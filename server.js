049eb2cea566d94aaa4160d4ba248aef53de5907
Ekaterina L <tarrenfuere@gmail.com>
2018-04-27 19:06:53 +0300

    another port

--- server.js
+++ server.js
@@ -80,6 +80,6 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-app.listen(80, () => {
-  console.log('server is running on port: 80');
+app.listen(3000, () => {
+  console.log('server is running on port: 3000');
 });
